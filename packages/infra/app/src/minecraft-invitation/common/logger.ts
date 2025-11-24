21e606cceefd445dede9efb9044b5ed3fffdfeae
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-24 17:16:30 +1100

    configure eslint to make handlers lint pass and logger util using aws lambda powertools logger

---
+++ packages/infra/app/src/minecraft-invitation/common/logger.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@aws-lambda-powertools/logger'
+
+export function createLogger(serviceName: string): Logger {
+  const logLevel = (process.env.LOG_LEVEL || 'INFO') as
+    | 'DEBUG'
+    | 'INFO'
+    | 'WARN'
+    | 'ERROR'
+
+  return new Logger({
+    serviceName,
+    logLevel,
+  })
+}
