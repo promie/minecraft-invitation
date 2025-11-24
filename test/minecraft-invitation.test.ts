ea159782d0b82a0a3343c58c50f42442424d9b20
Promie Yutasane <promie.yutasane@gmail.com>
2025-11-24 11:38:05 +1100

    initialise a brand new CDK project

---
+++ test/minecraft-invitation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MinecraftInvitation from '../lib/minecraft-invitation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/minecraft-invitation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MinecraftInvitation.MinecraftInvitationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
